02395b49047d32d266c5438264d3f71577e58a14
Eric G <65147562+E-Gantz@users.noreply.github.com>
2020-12-06 12:09:24 -0700

    progress on parser

---
+++ Prob.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+
+
+public class Prob{
+    ArrayList<classLab> stuffToBePlaced = new ArrayList<classLab>();
+    ArrayList<courseSlot> courseSlots = new ArrayList<courseSlot>();
+    
+
+}
