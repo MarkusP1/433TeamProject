f0585a2374197724783d5ede07a0fb57101916a5
seanp <sean.park3@ucalgary.ca>
2020-12-07 09:28:06 -0700

    Better toString for Slot

--- Slot.java
+++ Slot.java
@@ -85,9 +85,10 @@ public class Slot {
     public String toString() {
     	String slotType = isLab ? "Lab" : "Lecture";
     	
-    	return slotType + "slot on "
+    	return slotType + " slot on "
     			+ days.toString() + " from "
     			+ startTime.toString() + " to "
-    			+ startTime.toString();
+    			+ startTime.toString() + " of "
+    			+ classes.toString();
     }
 }
